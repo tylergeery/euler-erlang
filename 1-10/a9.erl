c9ced629370e2e069eea4b4fd6e55c4d4376f87e
tylergeery <tyler.geery@yahoo.com>
2018-06-06 00:19:08 -0700

    finish 9 and start new format

---
+++ 1-10/a9.erl
@@ -0,0 +1,29 @@
+-module(a9).
+-export([product_py_triplet/1]).
+
+product_py_triplet(N) when N > 0 ->
+    {A, B, C} = py_triplet(N, 1, 2, N-3),
+    io:format("Winner: ~p,~p,~p~n", [A, B, C]),
+    A * B * C.
+
+validate(N, A, B, C) ->
+    P = A + B + C,
+    io:format("Validate: A: ~p, B: ~p, C: ~p~n", [A,B,C]),
+    case P == N of
+        true -> (A*A + B*B) == (C*C);
+        false -> false
+    end.
+
+
+next(N, A, B, C) ->
+    if
+        A > trunc(N/3) -> {0, 0, 0};
+        B >= C -> py_triplet(N, A+1, A+2, N-(2*A)-3);
+        true -> py_triplet(N, A, B+1, N-A-B-1)
+    end.
+
+py_triplet(N, A, B, C) ->
+    case validate(N, A, B, C) of
+        true -> {A, B, C};
+        false -> next(N, A, B, C)
+    end.
